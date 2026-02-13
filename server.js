0b9183c665865af9544056bf08691c8f1c68f397
marinasteinbach <marina_steinbach@estudante.sesisenai.org.br>
2026-02-13 11:47:58 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import fastify from 'fastify';
-const servidor = fastify();
+const servidor = fastify ();
+const obterReceitas = [
+    {id: 1, nome: 'Bolo de fuba', ingredientes: 5  },
+    {id: 2, nome: 'Bolo de chocolate', ingredientes:6},
+    {id: 3, nome: 'Panqueca', ingredientes:3}
+]
 servidor.get('/usuarios', () => {
-    return ' ola mundo!'
+    return obterReceitas
 })
-servidor.post('/usuarios', (request, reply) => {
-    return ' ola mundo!'
+const rece = [
+    {id: 1, nome: 'Bolo de fuba', ingredientes: 5 },
+    {id: 2, nome: 'Bolo de chocolate', ingredientes:6},
+    {id: 3, nome: 'Panqueca', ingredientes:3}
+]
+servidor.get('/receitas', () => {
+    return rece
 })
- servidor.listen({
-     port:3000
- })
- 
+servidor.listen({
+    port:3000
+})
